417050d7544483ae1d5654468ce092fb5d6a797d
gulnaz <gulnaz.idrisova.05@bk.ru>
2025-05-24 17:46:59 +0300

    Добавили функцию контактной формы

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,7 @@ const contactForm = document.getElementById('contactForm');
 if (contactForm) {
     contactForm.addEventListener('submit', function(e) {
         e.preventDefault();
-        alert('Форма отправлена! (Это демо, данные никуда не отправляются)');
+        alert('Форма отправлена! (Данные никуда не отправляются)');
         this.reset();
     });
 }
